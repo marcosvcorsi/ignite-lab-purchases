e4dd460c6a069d1f303f9307ad4a6b7874e1fdea
Marcos Corsi <marcosvcorsi@gmail.com>
2022-04-05 08:19:30 -0300

    feat: add guard configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { HttpModule } from './http/http.module';
+import { TestController } from './test/test.controller';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    HttpModule,
+  ],
+  controllers: [TestController],
 })
 export class AppModule {}
---
+++ src/http/auth/authorization.guard.ts
@@ -0,0 +1,56 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+  Logger,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import jwt from 'express-jwt';
+import { expressJwtSecret } from 'jwks-rsa';
+import { promisify } from 'node:util';
+
+@Injectable()
+export class AuthorizationGuard implements CanActivate {
+  private logger;
+  private audience: string;
+  private domain: string;
+
+  constructor(configService: ConfigService) {
+    this.logger = new Logger(AuthorizationGuard.name);
+
+    this.audience = configService.get('AUTH0_AUDIENCE');
+    this.domain = configService.get('AUTH0_DOMAIN');
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const httpContext = context.switchToHttp();
+
+    const req = httpContext.getRequest();
+    const res = httpContext.getResponse();
+
+    const checkJwt = promisify(
+      jwt({
+        secret: expressJwtSecret({
+          cache: true,
+          rateLimit: true,
+          jwksRequestsPerMinute: 5,
+          jwksUri: `https://${this.domain}/.well-known/jwks.json`,
+        }),
+        audience: this.audience,
+        issuer: this.domain,
+        algorithms: ['RS256'],
+      }),
+    );
+
+    try {
+      await checkJwt(req, res);
+
+      return true;
+    } catch (error) {
+      this.logger.error(error);
+
+      throw new UnauthorizedException();
+    }
+  }
+}
