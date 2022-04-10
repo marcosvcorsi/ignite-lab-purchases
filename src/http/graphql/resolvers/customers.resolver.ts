b4077fdd82b77e29fbcbd3f9fa95c046a8814a09
Marcos Corsi <marcosvcorsi@gmail.com>
2022-04-09 21:16:52 -0300

    feat: adjust to apollo federation

--- src/http/graphql/resolvers/customers.resolver.ts
+++ src/http/graphql/resolvers/customers.resolver.ts
@@ -1,5 +1,11 @@
 import { UseGuards } from '@nestjs/common';
-import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import {
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+  ResolveReference,
+} from '@nestjs/graphql';
 import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
 import { AuthUser, CurrentUser } from 'src/http/auth/current-user';
 import { CustomersService } from 'src/services/customers.service';
@@ -22,5 +28,10 @@ export class CustomersResolver {
   @ResolveField()
   async purchases(@Parent() customer: Customer) {
     return this.purchasesService.findByCustomerId(customer.id);
+  }
+
+  @ResolveReference()
+  async resolveReference(reference: { authUserId: string }) {
+    return this.customersService.findByAuthUserId(reference.authUserId);
   }
 }
--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver } from '@nestjs/apollo';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 import path from 'node:path';
 import { ProductsResolver } from './graphql/resolvers/products.resolver';
 import { DatabaseModule } from '../database/database.module';
@@ -13,9 +16,9 @@ import { MessagingModule } from 'src/messaging/messaging.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
-      driver: ApolloDriver,
+      driver: ApolloFederationDriver,
     }),
     DatabaseModule,
     MessagingModule,
